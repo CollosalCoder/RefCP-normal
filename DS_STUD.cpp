a65bd59adb31a8557ca223df4151ee9fb91f8fd0
CollosalCoder <73419884+CollosalCoder@users.noreply.github.com>
2021-07-10 22:49:06 +0530

    Update DS_STUD.cpp
    Added remaining data structures (includes most known containers, welcome to add/edit any more in accordance with latest c++ standards).
    P.S: Policy-based data structure is present in a separate file.

--- DS_STUD.cpp
+++ DS_STUD.cpp
@@ -83,9 +83,10 @@ cout<<c<<"\n"; // tiva        */
     m.insert(pair<string,int>("babushka",23));
     for(auto i : m)
     cout<<i.first<<"\t"<<i.second<<"\n";      // keys are arranged in dictionary order   */
-// bitset : hihgly memory efficient since each element takes one bit of memory
-//for comparison an int array of size n takes 32n bits of memory whereas bitset takes n bits of memory
-    bitset<10> s;   // On declaration, all elements are set to 0 by default
+// bitset : higly memory efficient since each element takes one bit of memory
+// But size to be known at compile time !
+// for comparison an int array of size n takes 32n bits of memory whereas bitset takes n bits of memory
+/*   bitset<10> s;   // On declaration, all elements are set to 0 by default
     s[3]=1;
     s[5]=1;
     s[2]=1;
@@ -93,7 +94,7 @@ cout<<c<<"\n"; // tiva        */
     cout<<s.count()<<"\n";    // 4 i.e. count() gives back the number of ones in the set
     cout<<s[4]<<"\t"<<s[5]<<"\t"<<s[6]<<"\n";
 // If we want a completely defined bitset
-    bitset<10> s1(string("0010011010")); // allocation of bits from right to left
+    bitset<10> s1(string("0010011010")); // ```allocation of bits from right to left !!!```
     cout << s1[4] << "\n"; // 1
     cout << s1[5] << "\n"; // 0
 // All bit operations can be performed on the bitset directly
@@ -102,5 +103,71 @@ cout<<c<<"\n"; // tiva        */
     cout << (a&b) << "\n"; // 0010010000
     cout << (a|b) << "\n"; // 1011111110
     cout << (a^b) << "\n"; // 1001101110
+    // s1 = 0010011010
+    s1.set(0);        // by deafault set() sets bit to 1
+    s1.set(7,0);      // else set value can be passed
+    s1.set(9,1);      // 1000011011
+    cout<<s1<<"\n";   // bits are displayed as assigned, Note only the array-style indexing has been reversed
+    s1.flip(8);       // flips the bit at given index -> 1100011011
+    s1.flip();        // flips the entire set if no argument is passed
+    cout<<s1<<"\n";   // 0011100100
+    s1.reset();       // reset() performs literally what you wrote :)
+    cout<<s1<<"\n";   // 0000000000
+    if(a.any())                                   // any() returns 1 if bitset has atleast one set bit
+    cout<<"'a' has some(atleast 1) set bit\n";
+    if(!b.none())                                 // none() returns 1 if bitset has NO set bits
+    cout<<"'b' has some(atleast 1) set bit\n";
+  ////// Converting a decimal number to binary using bitset
+  int num=100;
+  bitset<8> g=bitset<8>(num);
+  cout<<"Binary representation of num=100 is "<<g<<"\nOr can be printed directly as "<<bitset<8>(num)<<"\n";   */
+// deque : like vector array, ```has all the functions of vector``` : benifit of push_front and pop_front
+// Refer Vectorstud for all operations.
+// demerits : slower than vector
+// Insertion and removal both take O(1) time.
+/*    deque<int> d;
+    d.push_back(5); // [5]
+    d.push_back(2); // [5,2]
+    d.push_front(3); // [3,5,2]
+    output(d);
+    d.pop_back(); // [3,5]
+    d.pop_front(); // [5]               */
+// Stack And Queue Sequential element access for all purposes
+// In stack only top element is accesible, whereas the top and bottom elements are accesible in a queue
+/*    stack<int> s;
+    s.push(3);
+    s.push(2);
+    s.push(5);
+    cout<<s.empty()<<endl;  // returns 0 as stack is not empty
+    cout << s.top()<<endl; // 5
+    s.pop();
+    cout << s.top()<<endl; // 2
+    queue<int> q;
+    q.push(3);
+    q.push(2);
+    q.push(5);
+    cout << q.front()<<endl; // 3
+    q.pop();
+    cout << q.front()<<endl; // 2                     */
+    // 2 more functions common to both are empty() and size() which return status(empty/not empty) and size respectively.
+// Priority Queue : similar to Ordered set (balanced binary tree), but works on a heap structure
+// Simple explanation:  execution(implementation) is similar to stack but defined priority
+// i.e. by default sorted in decreasing order (max heap)
+/*  priority_queue<int> q;
+// For smallest element (min heap)(Using a different priority order)
+// we can define as
+// priority_queue<int,vector<int>,greater<int>> q;
+      q.push(3);
+      q.push(5);
+      q.push(7);
+      q.push(2);
+      cout << q.top() << "\n"; // 7
+      q.pop();
+      cout << q.top() << "\n"; // 5
+      q.pop();
+      q.push(6);
+      cout << q.top() << "\n"; // 6
+      q.pop();         */
     return 0;
 }
+
